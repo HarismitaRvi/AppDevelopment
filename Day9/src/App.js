0eddb75577d526569217e79a643b0f475cdcd0f6
HarismitaRavi <126308644+HarismitaRvi@users.noreply.github.com>
2023-09-25 13:04:01 +0530

    Add files via upload

--- Day9/src/App.js
+++ Day9/src/App.js
@@ -1,47 +1,32 @@
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
-import "./App.css";
-import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Account } from "./pages/account/Account"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Regsiter } from "./pages/login/Regsiter"
+import { useSelector } from "react-redux"
 
-function App() {
+const App = () => {
+  const isLoggIn = useSelector((state) => state.auth.isLoggIn)
+  const cartItems = useSelector((state) => state.cart.itemsList)
+  console.log(cartItems)
   return (
-    <Router>
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    <>
+      {isLoggIn && (
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/regsiter' component={Regsiter} />
+            <Route exact path='/account' component={Account} />
+          </Switch>
+          <Footer />
+        </Router>
+      )}
+      {!isLoggIn && <Login />}
+    </>
+  )
 }
-
-export default App;
+export default App
--- Day9/src/index.js
+++ Day9/src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
+import { Provider } from "react-redux"
+import store from "./store"
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
